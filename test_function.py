0cc5f73bca4827a6c0a6401af253389d5447d42f
nadjano <nnolte@ebi.ac.uk>
2025-02-26 12:06:39 +0100

    Remove unnecessary blank lines in test_function.py

--- test_function.py
+++ test_function.py
@@ -7,8 +7,6 @@ def concat_strings(a, b):
     """
     return a + b
 
-
-
 def test_concat_strings():
     assert concat_strings('hello', 'world') == 'helloworld'
     assert concat_strings('hello', 'world') == 'hello world'
